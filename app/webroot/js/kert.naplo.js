b5b871f018fa09dfb3bbee8e424bf92c67ec700c
rrd@1108.cc <rrd@1108.cc@cf9bdf81-4ace-923d-6450-680ba651c8fe>
2012-06-15 12:42:03 +0000

    előző kikommentelt rész visszaállítása

--- app/webroot/js/kert.naplo.js
+++ app/webroot/js/kert.naplo.js
@@ -14,15 +14,15 @@ kert.naplo = {
 	
 	init : function(){
 		kert.naplo.inputok.datum = $('NaploDatum');
-		//kert.naplo.inputok.koltseg = $('NaploKoltseg');
+		kert.naplo.inputok.koltseg = $('NaploKoltseg');
 		kert.naplo.inputok.ora = $('NaploOra');
 		kert.naplo.inputok.munkas = $('NaploMunkas');
 		kert.naplo.inputok.munkasId = $('NaploMunkasId');
 		
 		kert.naplo.osszktg = $('osszktg');
 		kert.naplo.oradij = $('oradij');
-		/*kert.naplo.hely = $('NaploHelyId');
-		kert.naplo.termeny = $('NaploTermenyId');*/
+		kert.naplo.hely = $('NaploHelyId');
+		kert.naplo.termeny = $('NaploTermenyId');
 		
 		new Inputmask('date_iso', kert.naplo.inputok.datum);
 		//new Inputmask('amount_eu', kert.naplo.inputok.koltseg);
@@ -42,25 +42,25 @@ kert.naplo = {
 		kert.naplo.osszktg.update(number_format(osszeg, 0, ',', ' '));
 	},//kert.naplo.osszegFrissit
 
-	/*getOradij : function(e){
+	getOradij : function(e){
 		var selected = kert.naplo.inputok.munkas.selectedIndex;
 		kert.naplo.oradij.value = kert.naplo.inputok.munkas.options[selected].parentNode.label;
-	},//kert.naplo.getOradij*/
+	},//kert.naplo.getOradij
 	
-	/*munkashandler : function(azInput, aLi){
+	munkashandler : function(azInput, aLi){
 		//az aLi.id munkas_id _ oradik formátumú
 		var d = aLi.id.split('_');
 		kert.naplo.inputok.munkasId.value = d[0];
 		kert.naplo.oradij.value = d[1];
-	},//kert.naplo.munkashandler*/
+	},//kert.naplo.munkashandler
 	
-	/*helyhandler : function(azInput, aLi){
+	helyhandler : function(azInput, aLi){
 		kert.naplo.hely.value = aLi.id;
-	},//kert.naplo.helyhandler*/
+	},//kert.naplo.helyhandler
 
-	/*termenyhandler : function(azInput, aLi){
+	termenyhandler : function(azInput, aLi){
 		kert.naplo.termeny.value = aLi.id;
-	}//kert.naplo.helyhandler*/
+	}//kert.naplo.helyhandler
 }
 
 
